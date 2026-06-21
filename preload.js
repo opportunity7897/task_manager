0ab75c89d6d417661451a668e6dc7779b5b1fad0
Sota Amano <73663460+opportunity7897@users.noreply.github.com>
2026-06-21 17:29:47 +0900

    Hide local JSON path from header

--- preload.js
+++ preload.js
@@ -3,5 +3,5 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("localTaskApi", {
   loadDb: () => ipcRenderer.invoke("db:load"),
   saveDb: (data) => ipcRenderer.invoke("db:save", data),
-  getDataPath: () => ipcRenderer.invoke("db:path")
+  getDataPath: async () => "ローカルJSON保存"
 });
